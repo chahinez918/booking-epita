c1d959c07b6fd83344a7be9682303e3ab5e9f611
Chahinez <chahinez.berkani@epita.fr>
2026-04-15 10:41:20 +0200

    feat: credit card number masking

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
@@ -1,18 +1,14 @@
 package dev._xdbe.booking.creelhouse.infrastructure.persistence;
 
-
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.BadPaddingException;
 import javax.crypto.NoSuchPaddingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import dev._xdbe.booking.creelhouse.infrastructure.persistence.CryptographyHelper;
-
 
 @Converter
 public class CreditCardConverter implements AttributeConverter<String, String> {
@@ -38,9 +34,11 @@ public class CreditCardConverter implements AttributeConverter<String, String> {
 
     private String panMasking(String pan) {
         // Step 6:
-        return pan;
+        if (pan == null || pan.length() <= 8) return pan;
+        String first4 = pan.substring(0, 4);
+        String last4 = pan.substring(pan.length() - 4);
+        String middle = "*".repeat(pan.length() - 8);
+        return first4 + middle + last4;
         // Step 6: End
     }
-
-    
 }
